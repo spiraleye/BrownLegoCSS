a09ed153c96920476d7b97b51ea14e4271d81452
Johan Meiring <johan@spiraleye.com>
2012-12-27 17:49:17 +0200

    Additional tests and massive progress

--- BrownLegoCSS/BrownLegoCSS.go
+++ BrownLegoCSS/BrownLegoCSS.go
@@ -2,10 +2,10 @@ package BrownLegoCSS
 
 import (
 	"bytes"
-	//"fmt"
+	"fmt"
 	"regexp"
 	"strconv"
-	//"strings"
+	"strings"
 )
 
 type CssCompressor struct {
@@ -30,6 +30,9 @@ func (c *CssCompressor) extractDataUris() {
 		endIndex := index[1] - 1
 
 		// The below is (hopefully) the equivalent of Java's Matcher m.group(1)
+		// TODO:
+		// Probably not reliable for multiple matches... will need to look at
+		// using something like FindAllStringSubmatch()
 		terminator := re.FindStringSubmatch(string(tmpCss))[1] // ', " or empty (not quoted)
 
 		if len(terminator) == 0 {
@@ -79,8 +82,6 @@ func (c *CssCompressor) extractDataUris() {
 }
 
 func (c *CssCompressor) extractComments() {
-	//c.Css = []byte("/* This is a comment yo */ body { margin: 0px; } /* giggle */")
-
 	re, _ := regexp.Compile("\\/\\*[\\s\\w]+\\*\\/")
 
 	var sb bytes.Buffer
@@ -104,14 +105,313 @@ func (c *CssCompressor) extractComments() {
 		previousIndex = i[1]
 	}
 
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+
 	// Our string buffer is not empty, so something must have changed.
 	if sb.Len() > 0 {
 		c.Css = sb.Bytes()
 	}
 }
 
+func (c *CssCompressor) extractStrings() {
+	re, _ := regexp.Compile("(\"([^\\\\\"]|\\\\.|\\\\)*\")|('([^\\\\']|\\\\.|\\\\)*')")
+	re2, _ := regexp.Compile("(?i)progid:DXImageTransform.Microsoft.Alpha\\(Opacity=")
+
+	var sb bytes.Buffer
+	previousIndex := 0
+
+	indexes := re.FindAllIndex(c.Css, -1)
+	tokens := re.FindAllStringSubmatch(string(c.Css), -1)
+
+	for counter, i := range indexes {
+		if i[0] > 0 {
+			sb.WriteString(string(c.Css[previousIndex:i[0]]))
+		}
+		token := tokens[counter][0]
+		quote := token[0]
+		token = token[1 : len(token)-1]
+
+		// maybe the string contains a comment-like substring?
+		// one, maybe more? put'em back then
+		if strings.Index(token, "___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_") >= 0 {
+			max := len(c.comments)
+			for j := 0; j < max; j += 1 {
+				token = strings.Replace(token, "___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_"+strconv.Itoa(j)+"___", c.comments[j], -1)
+			}
+		}
+
+		// minify alpha opacity in filter strings
+		token = re2.ReplaceAllString(token, "alpha(opacity=")
+
+		c.preservedTokens = append(c.preservedTokens, token)
+		preserver := string(quote) + "___YUICSSMIN_PRESERVED_TOKEN_" + strconv.Itoa(len(c.preservedTokens)-1) + "___" + string(quote)
+		sb.WriteString(preserver)
+
+		previousIndex = i[1]
+	}
+
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+
+	// Our string buffer is not empty, so something must have changed.
+	if sb.Len() > 0 {
+		c.Css = sb.Bytes()
+	}
+}
+
+func (c *CssCompressor) processComments() {
+	max := len(c.comments)
+
+	for i := 0; i < max; i += 1 {
+		token := c.comments[i]
+		placeholder := "___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_" + strconv.Itoa(i) + "___"
+
+		// ! in the first position of the comment means preserve
+		// so push to the preserved tokens while stripping the !
+		if strings.Index(token, "!") == 0 {
+			c.preservedTokens = append(c.preservedTokens, token)
+			c.Css = bytes.Replace(c.Css, []byte(placeholder), []byte("___YUICSSMIN_PRESERVED_TOKEN_"+strconv.Itoa(len(c.preservedTokens)-1)+"___"), -1)
+			continue
+		}
+
+		// \ in the last position looks like hack for Mac/IE5/Opera
+		// shorten that to /*\*/ and the next one to /**/
+		// TODO: this doesn't seem to be working as intended, even in the Java version.
+		if strings.LastIndex(token, "\\") == len(token)-1 {
+			c.preservedTokens = append(c.preservedTokens, "\\")
+			c.Css = bytes.Replace(c.Css, []byte(placeholder), []byte("___YUICSSMIN_PRESERVED_TOKEN_"+strconv.Itoa(len(c.preservedTokens)-1)+"___"), -1)
+			i = i + 1 // attn: advancing the loop
+			c.preservedTokens = append(c.preservedTokens, "")
+			c.Css = bytes.Replace(c.Css, []byte("___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_"+strconv.Itoa(i)+"___"), []byte("___YUICSSMIN_PRESERVED_TOKEN_"+strconv.Itoa(len(c.preservedTokens)-1)+"___"), -1)
+			continue
+		}
+
+		// keep empty comments after child selectors (IE7 hack)
+		// e.g. html >/**/ body
+		if len(token) == 0 {
+			startIndex := bytes.Index(c.Css, []byte(placeholder))
+			if startIndex > 2 {
+				if c.Css[startIndex-3] == '>' {
+					c.preservedTokens = append(c.preservedTokens, "")
+					c.Css = bytes.Replace(c.Css, []byte(placeholder), []byte("___YUICSSMIN_PRESERVED_TOKEN_"+strconv.Itoa(len(c.preservedTokens)-1)+"___"), -1)
+				}
+			}
+		}
+
+		// in all other cases kill the comment
+		c.Css = bytes.Replace(c.Css, []byte("/*"+placeholder+"*/"), []byte(""), -1)
+	}
+}
+
+func (c *CssCompressor) performGeneralCleanup() {
+	//c.Css = []byte("p :link {\nba:zinga;;;\nfoo: bar;;;\n}")
+	//c.Css = []byte("p:first-letter{\nbuh: hum;\n}\np:first-line{\nbaa: 1;\n}\n\np:first-line,a,p:first-letter,b{\ncolor: red;\n}\n")
+	//c.Css = []byte("a { \n  margin: 0px 0pt 0em 0%;\n  _padding-top: 0ex;\n  background-position: 0 0;\n  padding: 0in 0cm 0mm 0pc\n}\n")
+	//c.Css = []byte("::selection { \n  margin: 0.6px 0.333pt 1.2em 8.8cm;\n}\n")
+	//c.Css = []byte(".color {\n  me: rgb(123, 123, 123);\n  }\n")
+	//c.Css = []byte(".foo, #AABBCC {\n  background-color:#aabbcc;\n  border-color:#Ee66aA #ABCDEF #FeAb2C;\n  filter:chroma(color = #FFFFFF );\n  filter:chroma(color=\"#AABBCC\");\n  filter:chroma(color='#BBDDEE');\n  color:#112233\n}\n")
+	c.Css = []byte(".color {\n  me: rgb(123, 123, 123);\n  impressed: #FfEedD;\n  again: #ABCDEF;\n  andagain:#aa66cc;\n  background-color:#aa66ccc;\n  filter: chroma(color=\"#FFFFFF\");\n  background: none repeat scroll 0 0 rgb(255, 0,0);\n  alpha: rgba(1, 2, 3, 4);\n  color:#1122aa\n}\n\n#AABBCC {\n  background-color:#ffee11;\n  filter: chroma(color = #FFFFFF );\n  color:#441122;\n  foo:#00fF11 #ABC #AABbCc #123344;\n  border-color:#aa66ccC\n}\n\n.foo #AABBCC {\n  background-color:#fFEe11;\n  color:#441122;\n  border-color:#AbC;\n  filter: chroma(color= #FFFFFF)\n}\n\n.bar, #AABBCC {\n  background-color:#FFee11;\n  border-color:#00fF11 #ABCDEF;\n  filter: chroma(color=#11FFFFFF);\n  color:#441122;\n}\n\n.foo, #AABBCC.foobar {\n  background-color:#ffee11;\n  border-color:#00fF11 #ABCDEF #AABbCc;\n  color:#441122;\n}\n\n@media screen {\n    .bar, #AABBCC {\n      background-color:#ffEE11;\n      color:#441122\n    }\n}\n")
+
+	// This function does a lot, ok?
+
+	// Remove the spaces before the things that should not have spaces before them.
+	// But, be careful not to turn "p :link {...}" into "p:link{...}"
+	// Swap out any pseudo-class colons with the token, and then swap back.
+	re, _ := regexp.Compile("(^|\\})(([^\\{:])+:)+([^\\{]*\\{)")
+	var sb bytes.Buffer
+	previousIndex := 0
+	indexes := re.FindAllIndex(c.Css, -1)
+	groups := re.FindAllStringSubmatch(string(c.Css), -1)
+	for counter, i := range indexes {
+		if i[0] > 0 {
+			sb.WriteString(string(c.Css[previousIndex:i[0]]))
+		}
+		s := groups[counter][0]
+		s = strings.Replace(s, ":", "___YUICSSMIN_PSEUDOCLASSCOLON___", -1)
+		s = strings.Replace(s, "\\\\", "\\\\\\\\", -1)
+		s = strings.Replace(s, "\\$", "\\\\\\$", -1)
+		sb.WriteString(s)
+		previousIndex = i[1]
+	}
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+	if sb.Len() > 0 {
+		c.Css = sb.Bytes()
+	}
+	// Remove spaces before the things that should not have spaces before them.
+	re, _ = regexp.Compile("\\s+([!{};:>+\\(\\)\\],])")
+	c.Css = re.ReplaceAll(c.Css, []byte("$1"))
+	// bring back the colon
+	c.Css = bytes.Replace(c.Css, []byte("___YUICSSMIN_PSEUDOCLASSCOLON___"), []byte(":"), -1)
+
+	// retain space for special IE6 cases
+	re, _ = regexp.Compile(":first\\-(line|letter)(\\{|,)")
+	c.Css = re.ReplaceAll(c.Css, []byte(":first-$1 $2"))
+
+	// no space after the end of a preserved comment
+	c.Css = bytes.Replace(c.Css, []byte("*/ "), []byte("*/"), -1)
+
+	// If there is a @charset, then only allow one, and push to the top of the file.
+	re, _ = regexp.Compile("^(.*)(@charset \"[^\"]*\";)")
+	c.Css = re.ReplaceAll(c.Css, []byte("$2$1"))
+	re, _ = regexp.Compile("^(\\s*@charset [^;]+;\\s*)+")
+	c.Css = re.ReplaceAll(c.Css, []byte("$1"))
+
+	// Put the space back in some cases, to support stuff like
+	// @media screen and (-webkit-min-device-pixel-ratio:0){
+	re, _ = regexp.Compile("\\band\\(")
+	c.Css = re.ReplaceAll(c.Css, []byte("and ("))
+
+	// Remove the spaces after the things that should not have spaces after them.
+	re, _ = regexp.Compile("([!{}:;>+\\(\\[,])\\s+")
+	c.Css = re.ReplaceAll(c.Css, []byte("$1"))
+
+	// remove unnecessary semicolons
+	re, _ = regexp.Compile(";+}")
+	c.Css = re.ReplaceAll(c.Css, []byte("}"))
+
+	// Replace 0(px,em,%) with 0.
+	re, _ = regexp.Compile("([\\s:])(0)(px|em|%|in|cm|mm|pc|pt|ex)")
+	c.Css = re.ReplaceAll(c.Css, []byte("$1$2"))
+
+	// Replace 0 0 0 0; with 0.
+	re, _ = regexp.Compile(":0 0 0 0(;|})")
+	re2, _ := regexp.Compile(":0 0 0(;|})")
+	re3, _ := regexp.Compile(":0 0(;|})")
+	c.Css = re.ReplaceAll(c.Css, []byte(":0$1"))
+	c.Css = re2.ReplaceAll(c.Css, []byte(":0$1"))
+	c.Css = re3.ReplaceAll(c.Css, []byte(":0$1"))
+
+	sb.Reset()
+	re, _ = regexp.Compile("(?i)(background-position|transform-origin|webkit-transform-origin|moz-transform-origin|o-transform-origin|ms-transform-origin):0(;|})")
+	previousIndex = 0
+	indexes = re.FindAllIndex(c.Css, -1)
+	groups = re.FindAllStringSubmatch(string(c.Css), -1)
+	for counter, i := range indexes {
+		if i[0] > 0 {
+			sb.WriteString(string(c.Css[previousIndex:i[0]]))
+		}
+		s := strings.ToLower(groups[counter][1]) + ":0 0" + groups[counter][2]
+		sb.WriteString(s)
+		previousIndex = i[1]
+	}
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+	if sb.Len() > 0 {
+		c.Css = sb.Bytes()
+	}
+
+	// Replace 0.6 to .6, but only when preceded by : or a white-space
+	re, _ = regexp.Compile("(:|\\s)0+\\.(\\d+)")
+	c.Css = re.ReplaceAll(c.Css, []byte("$1.$2"))
+
+	// Shorten colors from rgb(51,102,153) to #336699
+	// This makes it more likely that it'll get further compressed in the next step.
+	sb.Reset()
+	re, _ = regexp.Compile("rgb\\s*\\(\\s*([0-9,\\s]+)\\s*\\)")
+	indexes = re.FindAllIndex(c.Css, -1)
+	groups = re.FindAllStringSubmatch(string(c.Css), -1)
+	for counter, i := range indexes {
+		if i[0] > 0 {
+			sb.WriteString(string(c.Css[previousIndex:i[0]]))
+		}
+		rgbcolors := strings.Split(groups[counter][1], ",")
+		var hexcolor bytes.Buffer
+		hexcolor.WriteString("#")
+		for _, colour := range rgbcolors {
+			val, _ := strconv.Atoi(colour)
+			if val < 16 {
+				hexcolor.WriteString("0")
+			}
+			hexcolor.WriteString(fmt.Sprintf("%x", val))
+		}
+		sb.WriteString(hexcolor.String())
+		previousIndex = i[1]
+	}
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+	if sb.Len() > 0 {
+		c.Css = sb.Bytes()
+	}
+
+	// Shorten colors from #AABBCC to #ABC. Note that we want to make sure
+	// the color is not preceded by either ", " or =. Indeed, the property
+	//     filter: chroma(color="#FFFFFF");
+	// would become
+	//     filter: chroma(color="#FFF");
+	// which makes the filter break in IE.
+	// We also want to make sure we're only compressing #AABBCC patterns inside { }, not id selectors ( #FAABAC {} )
+	// We also want to avoid compressing invalid values (e.g. #AABBCCD to #ABCD)
+	sb.Reset()
+	re, _ = regexp.Compile("(\\=\\s*?[\"']?)?" + "#([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])([0-9a-fA-F])" + "(:?\\}|[^0-9a-fA-F{][^{]*?\\})")
+	previousIndex = 0
+
+	for match := re.Find(c.Css[previousIndex:]); match != nil; match = re.Find(c.Css[previousIndex:]) {
+		index := re.FindIndex(c.Css[previousIndex:])
+		submatches := re.FindStringSubmatch(string(c.Css[previousIndex:]))
+		submatchIndexes := re.FindSubmatchIndex(c.Css[previousIndex:])
+
+		sb.WriteString(string(c.Css[previousIndex : index[0]+len(c.Css[:previousIndex])]))
+
+		//boolean isFilter = (m.group(1) != null && !"".equals(m.group(1)));
+		// I hope the below is the equivalent of the above :P
+		isFilter := submatches[1] != "" && submatchIndexes[1] != -1
+
+		if isFilter {
+			// Restore, as is. Compression will break filters
+			sb.WriteString(submatches[1] + "#" + submatches[2] + submatches[3] + submatches[4] + submatches[5] + submatches[6] + submatches[7])
+		} else {
+			if strings.ToLower(submatches[2]) == strings.ToLower(submatches[3]) &&
+				strings.ToLower(submatches[4]) == strings.ToLower(submatches[5]) &&
+				strings.ToLower(submatches[6]) == strings.ToLower(submatches[7]) {
+				// #AABBCC pattern
+				sb.WriteString("#" + strings.ToLower(submatches[3]+submatches[5]+submatches[7]))
+			} else {
+				// Non-compressible color, restore, but lower case.
+				sb.WriteString("#" + strings.ToLower(submatches[2]+submatches[3]+submatches[4]+submatches[5]+submatches[6]+submatches[7]))
+			}
+		}
+
+		// The "+ 4" below is a crazy hack which will come back to haunt me later.
+		// For now, it makes everything work 100%.
+		previousIndex = submatchIndexes[7] + len(c.Css[:previousIndex]) + 4
+
+	}
+	if previousIndex > 0 {
+		sb.WriteString(string(c.Css[previousIndex:]))
+	}
+	if sb.Len() > 0 {
+		c.Css = sb.Bytes()
+	}
+
+	fmt.Printf("%s", c.Css)
+
+}
+
 func (c *CssCompressor) Compress() string {
 	c.extractDataUris()
 	c.extractComments()
+
+	// preserve strings so their content doesn't get accidentally minified
+	c.extractStrings()
+
+	// strings are safe, now wrestle the comments
+	c.processComments()
+
+	// Normalize all whitespace strings to single spaces. Easier to work with that way.
+	re, _ := regexp.Compile("\\s+")
+	c.Css = re.ReplaceAll(c.Css, []byte(" "))
+
+	// Do lots and lots and lots of fun things
+	// TODO: write/copy lots of tests for the function below.
+	// TODO: finish the function below :P
+	c.performGeneralCleanup()
+
 	return "\n"
 }
--- BrownLegoCSS/BrownLegoCSS_test.go
+++ BrownLegoCSS/BrownLegoCSS_test.go
@@ -47,8 +47,8 @@ func testExtractComments(t *testing.T) {
 			make([]string, 0),
 		},
 		{
-			"body { margin: 0px; } /* giggle */",
-			"body { margin: 0px; } /*___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_0___*/",
+			"body { margin: 0px; } /* giggle */ test",
+			"body { margin: 0px; } /*___YUICSSMIN_PRESERVE_CANDIDATE_COMMENT_0___*/ test",
 			[]string{" giggle "},
 		},
 		{
@@ -64,10 +64,88 @@ func testExtractComments(t *testing.T) {
 		compressor.extractComments()
 		got := string(compressor.Css)
 		if got != c.want {
-			t.Errorf("css: testExtractComments(%q) == (%q), want %q", c.s, got, c.want)
+			t.Errorf("css: extractComments(%q) == (%q), want %q", c.s, got, c.want)
 		}
 		if fmt.Sprintf("%v", compressor.comments) != fmt.Sprintf("%v", c.comments) {
-			t.Errorf("comments: testExtractComments(%q) == (%q), want %q", c.s, compressor.comments, c.comments)
+			t.Errorf("comments: extractComments(%q) == (%q), want %q", c.s, compressor.comments, c.comments)
+		}
+	}
+}
+
+func testExtractStrings(t *testing.T) {
+	var tests = []struct {
+		s, want         string
+		preservedTokens []string
+	}{
+		{
+			"No strings here hooray",
+			"No strings here hooray",
+			make([]string, 0),
+		},
+		{
+			"123 this \"is a test\" blah",
+			"123 this \"___YUICSSMIN_PRESERVED_TOKEN_0___\" blah",
+			[]string{"is a test"},
+		},
+		{
+			"123 this \"is a \\\"test\\\"\" blah",
+			"123 this \"___YUICSSMIN_PRESERVED_TOKEN_0___\" blah",
+			[]string{"is a \\\"test\\\""},
+		},
+		{
+			"123 this 'is a test' blah",
+			"123 this '___YUICSSMIN_PRESERVED_TOKEN_0___' blah",
+			[]string{"is a test"},
+		},
+		{
+			"123 this 'is a \\'test\\'' blah",
+			"123 this '___YUICSSMIN_PRESERVED_TOKEN_0___' blah",
+			[]string{"is a \\'test\\'"},
+		},
+		{
+			"mix \"them\" 'up' yo",
+			"mix \"___YUICSSMIN_PRESERVED_TOKEN_0___\" '___YUICSSMIN_PRESERVED_TOKEN_1___' yo",
+			[]string{"them", "up"},
+		},
+		{
+			"\"progid:DXImageTransform.Microsoft.Alpha(Opacity=250\"",
+			"\"___YUICSSMIN_PRESERVED_TOKEN_0___\"",
+			[]string{"alpha(opacity=250"},
+		},
+	}
+	for i, c := range tests {
+		compressor := CssCompressor{}
+		compressor.Css = []byte(tests[i].s)
+		compressor.extractStrings()
+		got := string(compressor.Css)
+		if got != c.want {
+			t.Errorf("css: extractStrings(%q) == (%q), want %q", c.s, got, c.want)
+		}
+		if fmt.Sprintf("%v", compressor.preservedTokens) != fmt.Sprintf("%v", c.preservedTokens) {
+			t.Errorf("preservedTokens: extractStrings(%q) == (%q), want %q", c.s, compressor.preservedTokens, c.preservedTokens)
+		}
+	}
+}
+
+func testProcessComments(t *testing.T) {
+	var tests = []struct {
+		s, want string
+	}{
+		{"No comment", "No comment"},
+		{"/* Normal comment */ test", " test"},
+		{"123 /*! Preserve comment */", "123 /*! Preserve comment */"},
+		{"123 /* Multiple */ comment /* Comments */", "123  comment "},
+		// Not quite sure if the below is correct...
+		{"/* Hack comment \\*/ /* asdf */", "/* Hack comment \\*/ "},
+	}
+	for i, c := range tests {
+		compressor := CssCompressor{}
+		compressor.Css = []byte(tests[i].s)
+		compressor.extractComments()
+		compressor.processComments()
+		got := string(compressor.Css)
+		if got != c.want {
+			t.Errorf("css: processComments(%q) == (%q), want %q", c.s, got, c.want)
 		}
 	}
 }
@@ -75,4 +153,6 @@ func testExtractComments(t *testing.T) {
 func Test(t *testing.T) {
 	testExtractDataUris(t)
 	testExtractComments(t)
+	testExtractStrings(t)
+	testProcessComments(t)
 }
