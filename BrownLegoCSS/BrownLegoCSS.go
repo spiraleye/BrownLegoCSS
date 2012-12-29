b01cee573c97b19e40aed581668a7dea8895a9b8
Johan Meiring <johan@spiraleye.com>
2012-12-29 21:29:29 +0200

    extractDataUris() rewritten. All tests pass now :)

--- BrownLegoCSS/BrownLegoCSS.go
+++ BrownLegoCSS/BrownLegoCSS.go
@@ -18,43 +18,38 @@ func (c *CssCompressor) extractDataUris() {
 	re, _ := regexp.Compile("url\\(\\s*([\"']?)data\\:")
 	re2, _ := regexp.Compile("\\s+")
 
-	maxIndex := len(c.Css) - 1
-	appendIndex := 0
-
-	tmpCss := c.Css
 	var sb bytes.Buffer
+	appendIndex := 0
+	maxIndex := len(c.Css) - 1
 
-	for match := re.Find(tmpCss); match != nil; match = re.Find(tmpCss) {
-		index := re.FindIndex(tmpCss)
-		startIndex := index[0] + 4 // length of string "url("
-		endIndex := index[1] - 1
+	indexes := re.FindAllIndex(c.Css, -1)
+	submatches := re.FindAllStringSubmatch(string(c.Css), -1)
 
-		// The below is (hopefully) the equivalent of Java's Matcher m.group(1)
-		// TODO:
-		// Probably not reliable for multiple matches... will need to look at
-		// using something like FindAllStringSubmatch()
-		terminator := re.FindStringSubmatch(string(tmpCss))[1] // ', " or empty (not quoted)
+	for counter, i := range indexes {
+		startIndex := i[0] + 4 // "url(".length()
+		endIndex := i[1] - 1
 
+		terminator := submatches[counter][1] // ', " or empty (not quoted)
 		if len(terminator) == 0 {
 			terminator = ")"
 		}
-
 		foundTerminator := false
+
 		for foundTerminator == false && endIndex+1 <= maxIndex {
-			endIndex = bytes.IndexByte(tmpCss[endIndex+1:], terminator[0]) + len(tmpCss[:endIndex]) + 1
-			if (endIndex > 0) && (string(tmpCss[endIndex-1]) != "\\") {
+			endIndex = bytes.IndexByte(c.Css[endIndex+1:], terminator[0]) + len(c.Css[:endIndex]) + 1
+			if (endIndex > 0) && (string(c.Css[endIndex-1]) != "\\") {
 				foundTerminator = true
 				if terminator != ")" {
-					endIndex = bytes.IndexByte(tmpCss[endIndex:], ')') + len(tmpCss[:endIndex])
+					endIndex = bytes.IndexByte(c.Css[endIndex:], ')') + len(c.Css[:endIndex])
 				}
 			}
 		}
 
 		// Enough searching, start moving stuff over to the buffer
-		sb.WriteString(string(tmpCss[appendIndex:index[0]]))
+		sb.WriteString(string(c.Css[appendIndex:i[0]]))
 
 		if foundTerminator {
-			var token string = string(tmpCss[startIndex:endIndex])
+			var token string = string(c.Css[startIndex:endIndex])
 			token = re2.ReplaceAllString(token, "")
 			c.preservedTokens = append(c.preservedTokens, token)
 
@@ -64,15 +59,13 @@ func (c *CssCompressor) extractDataUris() {
 			appendIndex = endIndex + 1
 		} else {
 			// No end terminator found, re-add the whole match. Should we throw/warn here?
-			sb.WriteString(string(tmpCss[index[0]:index[1]]))
-			appendIndex = index[1]
+			sb.WriteString(string(c.Css[i[0]:i[1]]))
+			appendIndex = i[1]
 		}
-
-		sb.WriteString(string(tmpCss[appendIndex:]))
-
-		tmpCss = tmpCss[appendIndex:]
 	}
-
+	if appendIndex > 0 {
+		sb.WriteString(string(c.Css[appendIndex:]))
+	}
 	// Our string buffer is not empty, so something must have changed.
 	if sb.Len() > 0 {
 		c.Css = sb.Bytes()
